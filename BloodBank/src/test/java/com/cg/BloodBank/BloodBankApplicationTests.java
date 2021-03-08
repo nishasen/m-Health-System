e00274dff87226fe5510f3731e92dc2e901714d6
nishasen <43262505+nishasen@users.noreply.github.com>
2021-03-08 13:22:43 +0530

    Add files via upload

---
+++ BloodBank/src/test/java/com/cg/BloodBank/BloodBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.BloodBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
