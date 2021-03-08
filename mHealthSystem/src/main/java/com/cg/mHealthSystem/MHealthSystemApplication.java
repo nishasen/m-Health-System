5e0f0439c4d383be6c24f47b24a12702e29b3677
nishasen <43262505+nishasen@users.noreply.github.com>
2021-03-08 13:21:21 +0530

    Add files via upload

---
+++ mHealthSystem/src/main/java/com/cg/mHealthSystem/MHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cg.mHealthSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MHealthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MHealthSystemApplication.class, args);
+	}
+
+}
---
+++ mHealthSystem/src/test/java/com/cg/mHealthSystem/MHealthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.mHealthSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MHealthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
